b8548a04877c8db7ce3a77be2533ed6be0d3b67d
Ozan Eşki <34617144+ozaneski13@users.noreply.github.com>
2021-11-01 10:50:44 +0300

    Enemy interest timer added.
    If player got detected by enemy, enemy follows player until interest time is over. Enemy.cs is removed for now since we don't need to get Enemy options from save file.

--- Assets/Scripts/Enemy/Enemy_Movement.cs
+++ Assets/Scripts/Enemy/Enemy_Movement.cs
@@ -1,15 +1,31 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Enemy_Movement : MonoBehaviour
 {
-    [SerializeField] private Transform _player = null;
     [SerializeField] private NavMeshAgent _navMeshAgent = null;
 
     [SerializeField] private float _lookRadius = 10f;
     [SerializeField] private float _rotationVariable = 5f;
+    [SerializeField] private float _interestTime = 3f;
+
+    private Transform _player = null;
+
+    private Vector3 _startingPosition = Vector3.zero;
+
+    private IEnumerator _chaseRoutine = null;
 
     private float _distance = 0f;
+
+    private bool _isChasing = false;
+
+    private void Start()
+    {
+        _player = Player.Instance.transform;
+
+        _startingPosition = transform.position;
+    }
 
     private void Update()
     {
@@ -17,7 +33,11 @@ public class Enemy_Movement : MonoBehaviour
 
         if (_distance <= _lookRadius)
         {
-            _navMeshAgent.SetDestination(_player.position);
+            if (!_isChasing)
+            {
+                _chaseRoutine = ChaseRoutine();
+                StartCoroutine(_chaseRoutine);
+            }
 
             if (_distance <= _navMeshAgent.stoppingDistance)
             {
@@ -27,12 +47,37 @@ public class Enemy_Movement : MonoBehaviour
         }
     }
 
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+
     private void FaceTarget()
     {
         Vector3 direction = (_player.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
 
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * _rotationVariable);
+    }
+
+    private IEnumerator ChaseRoutine()
+    {
+        _isChasing = true;
+
+        float passedTime = 0f;
+
+        while (passedTime < _interestTime)
+        {
+            passedTime += Time.deltaTime;
+
+            _navMeshAgent.SetDestination(_player.position);
+
+            yield return null;
+        }
+
+        _isChasing = false;
+
+        _navMeshAgent.SetDestination(_startingPosition);
     }
 
     private void OnDrawGizmosSelected()
