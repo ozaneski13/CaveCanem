da2da8ab22958e545b7002f1aaefa633f97fb1cb
Ozan Eşki <34617144+ozaneski13@users.noreply.github.com>
2021-11-24 00:05:40 +0300

    New player added to scene. Player anims added to Player_Movement.

--- Assets/Scripts/PlayerScripts/Player_Movement.cs
+++ Assets/Scripts/PlayerScripts/Player_Movement.cs
@@ -4,6 +4,8 @@ public class Player_Movement : MonoBehaviour
 {
     [SerializeField] private CharacterController _characterController = null;
 
+    [SerializeField] private Animator _animator = null;
+
     [SerializeField] private Transform _groundCheck = null;
     [SerializeField] private LayerMask _groundLayerMask;
 
@@ -30,25 +32,49 @@ public class Player_Movement : MonoBehaviour
 
         bool isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundLayerMask);
 
-        if (isGrounded && _velocity.y < 0)
-            _velocity.y = -2f;
+        if (isGrounded)
+        {
+            _animator.SetBool("Jump", false);
+            _animator.SetBool("Walk", false);
+            _animator.SetBool("Run", false);
+            _animator.SetBool("Idle", true);
+
+            if (_velocity.y < 0)
+                _velocity.y = -2f;
+        }
 
         float defaultSpeed = _normalSpeed;
 
         if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _animator.SetBool("Idle", false);
+            _animator.SetBool("Run", true);
             defaultSpeed = _highSpeed;
+        }
+
         if (Input.GetKey(KeyCode.LeftControl))
+        {
+            _animator.SetBool("Idle", false);
+            _animator.SetBool("Walk", true);//Slow walk anim
             defaultSpeed = _lowSpeed;
+        }
 
         float xPos = Input.GetAxis("Horizontal");
         float zPos = Input.GetAxis("Vertical");
+
+        if (zPos != 0)
+            _animator.SetBool("Idle", false);
+
         Vector3 moveDirection = transform.right * xPos + transform.forward * zPos;
         _characterController.Move(moveDirection * defaultSpeed * Time.deltaTime);
 
         float gravity = -9.81f;
 
         if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            _animator.SetBool("Jump", true);
             _velocity.y = Mathf.Sqrt(_jumpForce * -2f * gravity);
+        }
 
         _velocity.y += gravity * Time.deltaTime;
         _characterController.Move(_velocity * Time.deltaTime);
